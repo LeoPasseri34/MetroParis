79724bc53461c40ea6498d221d2b7941916b7174
lpass <s310842@studenti.polito.it>
2025-04-28 16:01:12 +0200

    costruzione grafo

--- database/DAO.py
+++ database/DAO.py
@@ -1,4 +1,5 @@
 from database.DB_connect import DBConnect
+from model.connessione import Connessione
 from model.fermata import Fermata
 
 
@@ -16,6 +17,59 @@ class DAO():
 
         for row in cursor:
             result.append(Fermata(**row))
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def hasConnessione(u: Fermata, v:Fermata):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = ("""Select *
+                 FROM connessione c
+                 WHERE c.id_stazP = %s and c.id_stazA = %s""")
+        cursor.execute(query, (u.id_fermata, v.id_fermata))
+
+        for row in cursor:
+            result.append(row)
+        cursor.close()
+        conn.close()
+        return len(result) > 0
+
+    @staticmethod
+    def getVicini(u: Fermata):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = ("""Select *
+                     FROM connessione c
+                     WHERE c.id_stazP = %s""")
+        cursor.execute(query, (u.id_fermata,))
+
+        for row in cursor:
+            result.append(Connessione(**row))
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getAllEdges():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = ("""Select *
+                         FROM connessione c""")
+        cursor.execute(query, )
+
+        for row in cursor:
+            result.append(Connessione(**row))
         cursor.close()
         conn.close()
         return result
--- model/model.py
+++ model/model.py
@@ -4,9 +4,47 @@ import networkx as nx
 class Model:
     def __init__(self):
         self._fermate = DAO.getAllFermate()
+        self._grafo = nx.DiGraph()
+        self._idMapFermate = {}
+        for f in self._fermate:
+            self._idMapFermate[f.id_fermata] = f
 
     def buildGraph(self):
-        pass
+        #aggiungiamo i nodi
+        self._grafo.add_nodes_from(self._fermate)
+        self.addEdges3()
+
+    def addEdges1(self):
+        """Aggiungo gli archi ciclando con doppio ciclo sui nodi
+        e testando se per ogni coppia esiste una connessione"""
+        for u in self._fermate:
+            for v in self._fermate:
+                if DAO.hasConnessione(u,v):
+                    self._grafo.add_edge(u,v)
+                    print("Aggiungo arco fra u e v", u, "e", v)
+
+
+    def addEdges2(self):
+        """Ciclo solo una volta e faccio una query per trovare
+        tutti i vicini"""
+        for u in self._fermate:
+            for con in DAO.getVicini(u):
+                v = self._idMapFermate[con.id_stazA]
+                self._grafo.add_edge(u,v)
+
+    def addEdges3(self):
+        allEdges = DAO.getAllEdges()
+        for e in allEdges:
+            u = self._idMapFermate[e.id_stazP]
+            v = self._idMapFermate[e.id_stazA]
+            self._grafo.add_edge(u,v)
+
+
+    def getNumNodi(self):
+        return len(self._grafo.nodes)
+
+    def getNumArchi(self):
+        return len(self._grafo.edges)
 
     @property
     def fermate(self):
